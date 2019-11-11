424cc8f5889cce17c416ffb8a6df0bf06c06d9a8
Elizabeth Tran <elizabethtran135@gmail.com>
2019-11-10 21:15:48 -0500

    Add javascript based on lecture

---
+++ src/main.js
@@ -0,0 +1,23 @@
+class Dessert {
+  constructor(type, calories) {
+    this.type = type;
+    this.year = calories;
+    console.log('Bake')
+  }
+}
+
+class Icecream extends Dessert {
+  constructor(type, calories, flavor, scoops) {
+    super(type, calories);
+    this.flavor = flavor;
+    this.scoops = scoops;
+  }
+
+  includeSpoon() {
+    console.log('Here is your spoon!');
+  }
+
+}
+const vanillaIceCream = new Icecream('Ice Cream', 340, 'Vanilla', 3)
+  console.log(vanillaIceCream);
+vanillaIceCream.includeSpoon();
